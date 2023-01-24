3d6b5fe863b179ae70115660139b3ce51debac8f
WeMadeIt91 <70448825+WeMadeIt91@users.noreply.github.com>
2023-01-24 11:28:48 -0500

    Add files via upload
    My first R Script! Finally learning R for Data Scientist/Analyst Career

---
+++ Sales_Calc_First_R_Calc.R
@@ -0,0 +1,8 @@
+# Shaquana's First R Calculations!
+
+quarter_1_sales <- 35657.98
+Quarter_2_sales <- 43810.55
+midyear_sales <- quarter_1_sales + Quarter_2_sales
+midyear_sales
+midend_sales <- midyear_sales * 2
+midend_sales
