ad7aeb6a925635af161ec6309d16bcbe46e8d5af
WeMadeIt91 <70448825+WeMadeIt91@users.noreply.github.com>
2023-01-28 14:20:04 -0500

    Update Data Frame Creation.R

--- Data Frame Creation.R
+++ Data Frame Creation.R
@@ -1,3 +1,11 @@
 library(tidyverse)
+#create a vector array named names  and age with 4 csv
 names <- c("Ginger", "Sarah", "George", "Terry")
 age <- c(2,6,12,24)
+people <- data.frame(names, age)
+Lemonade <- c("Strawberry", "Mango", "Pineapple", "Watermelon","Raspberry")  
+rank <- c(1,2,3,4,5)
+lemonadeBlends <- data.frame(Lemonade, rank)
+head(lemonadeBlends)
+str(lemonadeBlends)
+glimpse(lemonadeBlends)
